7fd40b35b357cd71ac86764404646c10b6b293ba
sam <samuel13241@gmail.com>
2023-04-04 16:49:59 -0700

    no message

---
+++ target/classes/static/js/main.js
@@ -0,0 +1,40 @@
+
+
+// slideshow_animation(sub-method)   dots can be null
+function slide_looping(slides,dots){
+    for(let i = 0; i<slides.length; i++){
+        setTimeout(function(){
+            if(!$(slides.eq(i)).hasClass('active')){
+                $(slides.eq(i)).addClass('active');
+                console.log(`slide-${i}activated`);
+                // for dots
+                console.log(dots!=null);
+                if(dots!=null){
+                    console.log(i+"th slide for testimonial");
+                    dots.eq(i).addClass('active');
+                    dots.eq(i).siblings().removeClass('active');
+                }
+
+            }
+            $(slides.eq(i)).siblings().removeClass('active');
+            // console.log(`slide-${i}removed`);
+        },4000*i);
+
+    }
+
+}
+
+
+
+ function main_page_slideshow_animation(){
+    let slides = $('.slide_wrap>*');
+    // let dots = $('.main-page__slide_dot >*');
+    slide_looping(slides,null);
+
+    setInterval(function(){
+        slide_looping(slides,null);
+    },12000);
+
+}
+
+main_page_slideshow_animation();
